4edac16973d7ae68c2514ae5a702a523a3a1be06
susho-magician <sushobhit.srivastava@esmagico.in>
2025-03-25 17:05:13 +0530

    Add files via upload

---
+++ src/data/managementSolutions.js
@@ -0,0 +1,42 @@
+export const managementTabs = [
+    {
+        id: 1, title: "PYVIT", image: "/assets/pyvit.png",
+        content: "PYVIT is a cutting-edge platform that allows influencers and creators to make videos instantly shoppable. With both mobile and desktop tools, it amplifies content across 50+ countries, boosting engagement, increasing click-through rates, and driving conversions for enhanced profitability.",
+        stats: [
+            { number: "50", description: "Countries audiences reached" },
+            { number: "80%", description: "Click-throughs increased" },
+            { number: "20%", description: "Commissions earned by influencres" },
+            { number: "20%", description: "Viewing time boost" }
+        ]
+    },
+    {
+        id: 2, title: "EG Parcel", image: "/assets/egparcel.png",
+        content: "EG Parcel is a comprehensive 3PL solution that provides efficient product storage and streamlined fulfillment. We manage inventory and offer fast, reliable shipping, allowing you to focus on business growth while we optimize your logistics and supply chain operations",
+        stats: [
+            { number: ">96.2%", description: "On-Time Delivery Rate" },
+            { number: "99%", description: "Shipping Accuracy" },
+            { number: "<48 hours", description: "Lead Time for Receiving (Industry Standard)" },
+            { number: "98%", description: "Fill Rate   (Industry Standard)" }
+        ]
+    },
+    {
+        id: 3, title: "Pigeon Slip", image: "/assets/pigeon.png",
+        content: "PigeonShip offers on-demand, same-day local delivery in most major markets at competitive rates—up to 50% below market. We handle everything from pickup to delivery, ensuring reliable service with real-time tracking. Trust us to enhance your delivery efficiency and customer satisfaction.",
+        stats: [
+            { number: "99.8%", description: "On-Time Delivery Rate" },
+            { number: "98.1%", description: "Success Rate" },
+            { number: "0", description: "Lost Package Claims" },
+            { number: "2x", description: "Gains in Customer Satisfaction" },
+            { number: "up to 50%", description: "Savings on your current shipping costs" }
+        ]
+    },
+    {
+        id: 4, title: "Epic Global", image: "/assets/epicGlobal.png",
+        content: "Epic Global is a world-leading brand accelerator that empowers businesses to scale globally through AI-driven insights and data-powered strategies. We manage eCommerce, optimize listings, protect brands, and handle global fulfillment, all with no fees, ensuring long-term growth and success.",
+        stats: [
+            "NTB Sales (New to Brand Sales)", "Customer Lifetime Vlaue (CLV)", "NTB Orders (New to Brand Orders)", "Sales",
+            "NTB Customers (New to Brand Customers)", "Repeat Customers", "Shows the total revenue a customer is expected to generate over their relationship with your brand",
+            "Conversion Rate", "Average Order Value (AOV)"
+        ]
+    },
+]
